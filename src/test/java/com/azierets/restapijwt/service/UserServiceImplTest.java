3331bcb16a62655ee8ea1130091394981104a4a3
Anatoli Aziarets <lilenoothher@gmail.com>
2021-08-14 17:41:58 +0300

    Add mockito.verify check

--- src/test/java/com/azierets/restapijwt/service/UserServiceImplTest.java
+++ src/test/java/com/azierets/restapijwt/service/UserServiceImplTest.java
@@ -5,6 +5,7 @@ import com.azierets.restapijwt.model.UserRole;
 import com.azierets.restapijwt.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -45,6 +46,8 @@ class UserServiceImplTest {
 
         assertEquals(UserRole.USER, user.getRole());
         assertTrue(pattern.matcher(user.getPassword()).find());
+
+        Mockito.verify(userRepository, Mockito.times(1)).save(user);
     }
 
     @Test
