241474efe7a587a0beb6a77948534a940fbd714f
aozerets <likenoothher@gmail.com>
2021-10-12 21:08:01 +0300

    Fix remarks of Redis commit

--- src/main/java/com/azierets/restapijwt/config/RedisConfig.java
+++ src/main/java/com/azierets/restapijwt/config/RedisConfig.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -23,17 +21,5 @@ public class RedisConfig {
         redisStandaloneConfiguration.setHostName(redisHost);
         redisStandaloneConfiguration.setPort(redisPort);
         return redisStandaloneConfiguration;
-    }
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory(redisStandaloneConfiguration());
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
     }
 }
